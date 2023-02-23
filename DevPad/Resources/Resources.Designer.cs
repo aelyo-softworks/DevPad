d89bc5657eb3fc3783598c041adfac8363ed07b0
Simon Mourier <simon_mourier@hotmail.com>
2023-02-23 17:22:11 +0100

    Added icon.

--- DevPad/Resources/Resources.Designer.cs
+++ DevPad/Resources/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace DevPad.Resources {
+namespace Resources {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace DevPad.Resources {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DevPad.Resources.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Resources.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
