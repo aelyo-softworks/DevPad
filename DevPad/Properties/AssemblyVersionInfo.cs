ee406307218294900291a74a473d1e1ffde3959e
Simon Mourier <simon_mourier@hotmail.com>
2023-05-06 18:08:55 +0200

    fixed monaco inclusion

--- DevPad/Properties/AssemblyVersionInfo.cs
+++ DevPad/Properties/AssemblyVersionInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.12")]
-[assembly: AssemblyInformationalVersion("1.0.0.12")]
+[assembly: AssemblyFileVersion("1.0.0.13")]
+[assembly: AssemblyInformationalVersion("1.0.0.13")]
 
-[assembly: AssemblyMetadata("Commit Date", "Fri, 05 May 2023 15:33:41 GMT")]
+[assembly: AssemblyMetadata("Commit Date", "Sat, 06 May 2023 16:08:55 GMT")]
