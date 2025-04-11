6ae7b4e7619060da1893811284431f64028c91c6
Simon Mourier <smourier@users.noreply.github.com>
2025-04-11 09:40:11 +0200

    wip

--- DevPad/Properties/AssemblyVersionInfo.cs
+++ DevPad/Properties/AssemblyVersionInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.16")]
-[assembly: AssemblyInformationalVersion("1.0.0.16")]
+[assembly: AssemblyFileVersion("1.0.0.18")]
+[assembly: AssemblyInformationalVersion("1.0.0.18")]
 
-[assembly: AssemblyMetadata("Commit Date", "Wed, 04 Sep 2024 17:14:14 GMT")]
+[assembly: AssemblyMetadata("Commit Date", "Fri, 11 Apr 2025 07:40:12 GMT")]
